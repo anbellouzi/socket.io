c526a18ad340d36aa000a448450fbc352d7811e7
Anas Bellouzi <anbellouzi@gmail.com>
2020-11-27 23:29:49 -0800

    first update

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ var http = require('http').Server(app);
 var io = require('./node_modules/socket.io')(http);
 var port = process.env.PORT || 3000;
 
+
+let onlineUsers = {};
+
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
@@ -11,7 +15,21 @@ io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
+
+
+  socket.on('new user', function(username) {
+    //Save the username as key to access the user's socket id
+    onlineUsers[username] = socket.id;
+    //Save the username to socket as well. This is important for later.
+    socket["username"] = username;
+    console.log(`✋ ${username} has joined the chat! ✋`);
+    io.emit("new user", username, {
+      // channels : channels,
+    });
+  })
+
 });
+
 
 http.listen(port, function(){
   console.log('listening on *:' + port);
